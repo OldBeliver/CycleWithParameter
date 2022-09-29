f88b7b7df093ca6cdb8f7575728135a187a54710
OldBeliver <380506338004@yandex.com>
2022-09-29 12:32:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CycleWithParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startPoint = 5;
+            int endPoint = 100;
+            int step = 7;
+
+            for (int i = startPoint; i < endPoint; i+=step)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
